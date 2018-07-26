14ee26dd3d196f1748662f3bffa16878ee439b3d
ChaosAD <frasker@hotmail.com>
2018-07-26 15:33:55 +0800

    Added SimpleArithmetic tests using real Tokenizer

--- test/test_SimpleArithmetic.c
+++ test/test_SimpleArithmetic.c
@@ -3,9 +3,9 @@
 #include "unity.h"
 #include "SimpleArithmetic.h"
 #include "Common.h"
-#include "Token.h"
 #include "Error.h"
-#include "mock_Tokenizer.h"
+#include "Token.h"
+#include "Tokenizer.h"
 #include "CException.h"
 #include "Exception.h"
 
@@ -13,101 +13,62 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_getToken_with_mock_given_23_plus_47() {
-  Token *token, *value1Token, *value2Token, *plusToken;
-  OperatorToken *opToken;
-  IntegerToken *intToken;
-  Tokenizer dummyTokenizer, *tokenizer = (Tokenizer *)&dummyTokenizer;
-  char *line = " 23 + 47 ";
-
-  value1Token = createIntegerToken(23, "23");
-  value2Token = createIntegerToken(47, "47");
-  plusToken = createOperatorToken("+", "+");
-
-  createTokenizer_ExpectAndReturn(line, tokenizer);
-  getToken_ExpectAndReturn(tokenizer, value1Token);
-  getToken_ExpectAndReturn(tokenizer, plusToken);
-  getToken_ExpectAndReturn(tokenizer, value2Token);
-
-  tokenizer = createTokenizer(line);
-  intToken = (IntegerToken *)getToken(tokenizer);
-
-  TEST_ASSERT_NOT_NULL(intToken);
-  TEST_ASSERT_EQUAL(TOKEN_INTEGER_TYPE, intToken->type);
-  TEST_ASSERT_EQUAL(23, intToken->value);
-  freeToken(intToken);
-
-  opToken = (OperatorToken *)getToken(tokenizer);
-  TEST_ASSERT_NOT_NULL(opToken);
-  TEST_ASSERT_EQUAL(TOKEN_OPERATOR_TYPE, opToken->type);
-  TEST_ASSERT_EQUAL_STRING("+", opToken->str);
-  freeToken(opToken);
-
-  intToken = (IntegerToken *)getToken(tokenizer);
-  TEST_ASSERT_NOT_NULL(intToken);
-  TEST_ASSERT_EQUAL(TOKEN_INTEGER_TYPE, intToken->type);
-  TEST_ASSERT_EQUAL(47, intToken->value);
-  freeToken(intToken);
-}
-
-void test_operateOn2Numbers_with_getToken_mock_given_987_multiply_161_expect_158907() {
+void test_operateOn2Numbers_with_multiplication() {
   CEXCEPTION_T e;
-  Token *token, *value1Token, *value2Token, *plusToken;
-  OperatorToken *opToken;
-  IntegerToken *intToken;
-  Tokenizer dummyTokenizer, *tokenizer = (Tokenizer *)&dummyTokenizer;
+  Token *token;
+  Tokenizer *tokenizer;
   int result;
-  char *line = " 987 * 161 ";     // This line is for readability only
 
-  // Fake values
-  value1Token = createIntegerToken(987, "987");
-  value2Token = createIntegerToken(161, "161");
-  plusToken = createOperatorToken("*", "*");
-  // Mock with the fake values
-  createTokenizer_ExpectAndReturn(line, tokenizer);
-  getToken_ExpectAndReturn(tokenizer, value1Token);
-  getToken_ExpectAndReturn(tokenizer, plusToken);
-  getToken_ExpectAndReturn(tokenizer, value2Token);
   // Run test
   Try {
-    tokenizer = createTokenizer(line);
+    tokenizer = createTokenizer(" 23 * 78 ");
     result = operateOn2Numbers(tokenizer);
-    printf("987 * 161 = %d\n", result);
-    TEST_ASSERT_EQUAL(158907, result);
+    printf("%s = %d\n", tokenizer->str, result);
+    TEST_ASSERT_EQUAL(23 * 78, result);
   } Catch(e) {
     // Catch and display exception if it has been thrown
-    printf("Err: %s", e->msg);
+    dumpTokenErrorMessage(e, 1);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
   }
+  freeTokenizer(tokenizer);
 }
 
-void test_operateOn2Numbers_with_getToken_mock_given_48_subtract_920_expect_minus872() {
+void test_operateOn2Numbers_with_subtracion() {
   CEXCEPTION_T e;
-  Token *token, *value1Token, *value2Token, *plusToken;
-  OperatorToken *opToken;
-  IntegerToken *intToken;
-  Tokenizer dummyTokenizer, *tokenizer = (Tokenizer *)&dummyTokenizer;
+  Token *token;
+  Tokenizer *tokenizer;
   int result;
-  char *line = " 48 - 920 ";     // This line is for readability only
 
-  // Fake values
-  value1Token = createIntegerToken(48, "48");
-  value2Token = createIntegerToken(920, "920");
-  plusToken = createOperatorToken("-", "-");
-  // Mock with the fake values
-  createTokenizer_ExpectAndReturn(line, tokenizer);
-  getToken_ExpectAndReturn(tokenizer, value1Token);
-  getToken_ExpectAndReturn(tokenizer, plusToken);
-  getToken_ExpectAndReturn(tokenizer, value2Token);
   // Run test
   Try {
-    tokenizer = createTokenizer(line);
+    tokenizer = createTokenizer(" 12 - 765678 ");
     result = operateOn2Numbers(tokenizer);
-    printf("48 - 920 = %d\n", result);
-    TEST_ASSERT_EQUAL(-872, result);
+    printf("%s = %d\n", tokenizer->str, result);
+    TEST_ASSERT_EQUAL(12 - 765678, result);
   } Catch(e) {
     // Catch and display exception if it has been thrown
-    printf("Err: %s", e->msg);
+    dumpTokenErrorMessage(e, 1);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
   }
+  freeTokenizer(tokenizer);
+}
+
+void test_operateOn2Numbers_with_divide() {
+  CEXCEPTION_T e;
+  Token *token;
+  Tokenizer *tokenizer;
+  int result;
+
+  // Run test
+  Try {
+    tokenizer = createTokenizer(" 456/   2 ");
+    result = operateOn2Numbers(tokenizer);
+    printf("%s = %d\n", tokenizer->str, result);
+    TEST_ASSERT_EQUAL(456/2, result);
+  } Catch(e) {
+    // Catch and display exception if it has been thrown
+    dumpTokenErrorMessage(e, 1);
+    TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
+  }
+  freeTokenizer(tokenizer);
 }
